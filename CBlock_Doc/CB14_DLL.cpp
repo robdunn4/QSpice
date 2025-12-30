6c2c7277ce3167e342f628e7c099f47cbd972f89
Robert Dunn <rediv@bellsouth.net>
2025-12-30 17:13:03 -0600

    Adding C-Block Basics #14 doc & misc updates

--- CBlock_Doc/CB14_DLL.cpp
+++ CBlock_Doc/CB14_DLL.cpp
@@ -1,30 +1,14 @@
-// Automatically generated C++ file on Tue Dec 16 15:12:38 2025
+//------------------------------------------------------------------------------
+// CB14_Dll.cpp -- Example code for C-Block Basics #14.
+//------------------------------------------------------------------------------
+// Full project code and documentation is available here:
+//    https://github.com/robdunn4/QSpice
 //
-// To build with Digital Mars C++ Compiler:
-//
+// This code compiles successfully using the Digital Mars C++ Compiler:
 //    dmc -mn -WD cb14_dll.cpp kernel32.lib
+//
 
 extern "C" __declspec(dllexport) void (*Display)(const char *format, ...) = 0;
-
-// clang-format off
-// extern "C" __declspec(dllexport) void (*EXIT)(const char *format, ...)          = 0; // print message like printf() but exit(0) afterward
-// extern "C" __declspec(dllexport) const double *DegreesC                         = 0; // pointer to current circuit temperature
-// extern "C" __declspec(dllexport) const int *StepNumber                          = 0; // pointer to current step number
-// extern "C" __declspec(dllexport) const int *NumberSteps                         = 0; // pointer to estimated number of steps
-// extern "C" __declspec(dllexport) const char* const *InstanceName                = 0; // pointer to address of instance name
-// extern "C" __declspec(dllexport) const char *QUX                                = 0; // path to QUX.exe
-// extern "C" __declspec(dllexport) const bool *ForKeeps                           = 0; // pointer to whether being evaluated non-hypothetically
-// extern "C" __declspec(dllexport) const bool *HoldICs                            = 0; // pointer to whether instance initial conditions are being held
-// extern "C" __declspec(dllexport) const void *GUI_HWND                           = 0; // pointer to Window handle of QUX.exe
-// extern "C" __declspec(dllexport) const double *CKTtime                          = 0;
-// extern "C" __declspec(dllexport) const double *CKTdelta                         = 0;
-// extern "C" __declspec(dllexport) const int *IntegrationOrder                    = 0;
-// extern "C" __declspec(dllexport) const char *InstallDirectory                   = 0;
-// extern "C" __declspec(dllexport) double (*EngAtof)(const char **string)         = 0;
-// extern "C" __declspec(dllexport) const char *(*BinaryFormat)(unsigned int data)                          = 0; // BinaryFormat(0x1C) returns "0b00011100"
-// extern "C" __declspec(dllexport) const char *(*EngFormat   )(double x, const char *units, int numDgts)   = 0; // EngFormat(1e-6, "s", 6) returns "1�s"
-// extern "C" __declspec(dllexport) int (*DFFT)(struct sComplex *u, bool inv, unsigned int N, double scale) = 0; // Discrete Fast Fourier Transform
-// clang-format on
 
 union uData {
   bool                   b;
@@ -56,12 +40,6 @@ int __stdcall DllMain(void *module, unsigned int reason, void *reserved) {
 
 extern "C" __declspec(dllexport) void cb14_dll(
     void **opaque, double t, union uData *data) {
-  // double      In      = data[0].d;     // input
-  // bool        bEnable = data[1].b;     // input parameter
-  // int         iGain   = data[2].i;     // input parameter
-  // const char *sVal    = data[3].str;   // input parameter
-  // double      fVal    = data[4].d;     // input parameter
-  // double     &Out     = data[5].d;     // output
   double      In     = data[0].d;     // input
   bool        Enable = data[1].b;     // input parameter
   int         Gain   = data[2].i;     // input parameter
